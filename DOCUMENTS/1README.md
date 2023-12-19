88f55006659dfe2ba1a740e279e8aeb2e30dd9fc
PitchLost <thundergames221@gmail.com>
2023-12-19 14:16:20 +1300

    First Commit

---
+++ DOCUMENTS/1README.md
@@ -0,0 +1,11 @@
+# Maze Game
+
+
+# The Game: 
+
+The user will have to navigate through a 2D maze. At the end of the maze will the the 'Tresure' and collecting that will move the user onto the next level.
+Hitting the wall will result in the user dying and having to restart the level. Each level will increase in difficulty and there will be 10 levels.
+
+
+# File Layout: 
+'main' will be used to call in functions from designated locations. There should be MINIMAL functions on the main file to maintain organisation. 
---
+++ main.py
@@ -0,0 +1,56 @@
+import pygame 
+import random
+
+
+# Global Variables: 
+
+# Colors: 
+class colors: 
+    RED = (255,0,0)
+    GREEN = (0,255,0)
+    BLUE = (0,0,255)
+    WHITE = (255,255,255)
+    BLACK = (0,0,0)
+
+# Data Collection: 
+class data_collection: 
+    loop_counter = 0
+    levels_cleared = 0
+    levels_cleared_names = []
+
+
+# Pygame Variables:
+pygame.init()
+
+
+# Screen Size: 
+width = 400
+height = 400
+
+# Create the game window
+screen = pygame.display.set_mode((width, height))
+
+# Game Loop 
+run = True # If this is set to true the game will be running
+
+while run == True: 
+
+    # Make the Background White
+    screen.fill(colors.WHITE)
+
+    # Update the display each tick
+    pygame.display.update()
+
+    # Event Listener: 
+    for event in pygame.event.get():
+        # Listen for the quit event 
+        if event == pygame.QUIT: 
+            run = False # Setting this to false will break the loop
+        # Any Other Event Listeners:
+
+
+
+
+
+
+pygame.quit()
